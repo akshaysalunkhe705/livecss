67d0f3741470a3c2c4dfcba998111711e100b573
akshaySalunkhe <akshay.salunkhe705@gmail.com>
2018-11-25 18:25:25 +0530

    On branch master  Your branch is up to date with 'origin/master'.

--- livecss.js
+++ livecss.js
@@ -44,7 +44,6 @@ function border()
 	let Values = initValues();
 	$("."+className).css({"border": Values.border.size+"px "+ Values.border.type+" #"+Values.border.color});
 }
-
 function backgroundColor()
 {
 	let className = getClassName();
@@ -57,25 +56,9 @@ function backgroundColor()
 
 function colorPicker()
 {
-	/**
- * jscolor - JavaScript Color Picker
- *
- * @link    http://jscolor.com
- * @license For open source use: GPLv3
- *          For commercial use: JSColor Commercial License
- * @author  Jan Odvarko
- * @version 2.0.5
- *
- * See usage examples at http://jscolor.com/examples/
- */
-
 
 "use strict";
-
-
 if (!window.jscolor) { window.jscolor = (function () {
-
-
 var jsc = {
 
 
